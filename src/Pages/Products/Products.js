29a6bbedeeddca6e408f9b76306faadff198055c
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-11-25 20:04:01 +0600

    modal added

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,32 +1,32 @@
 
-// import React, { useState } from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import ProductCard from './ProductCard';
-// import ProductModal from './ProductModal';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import ProductCard from './ProductCard';
+import ProductModal from './ProductModal';
 
-// const Products = () => {
-//     const category = useLoaderData()
-//     const [info, setInfo] = useState(null)
-
-
-//     return (
-//         <section>
-//             <div className='grid md:grid-cols-2 mt-10'>
-//                 {
-//                     category?.map(product => <ProductCard key={product._id} product={product}
-//                         setInfo={setInfo}
+const Products = () => {
+    const category = useLoaderData()
+    const [info, setInfo] = useState(null)
 
 
-//                     >
-//                     </ProductCard>)
-//                 }
+    return (
+        <section>
+            <div className='grid md:grid-cols-2 mt-10'>
+                {
+                    category?.map(product => <ProductCard key={product._id} product={product}
+                        setInfo={setInfo}
 
-//             </div>
-//             {info &&
-//                 <ProductModal info={info}></ProductModal>
-//             }
-//         </section>
-//     );
-// };
 
-// export default Products;
+                    >
+                    </ProductCard>)
+                }
+
+            </div>
+            {info &&
+                <ProductModal info={info}></ProductModal>
+            }
+        </section>
+    );
+};
+
+export default Products;
